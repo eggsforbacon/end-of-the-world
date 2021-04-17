7011e8ae27cd889cbb599663f333eb0abaa8b2f2
Samuel Hernandez <doicirohangnan@gmail.com>
2021-04-16 20:37:42 -0500

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Main {
+
+    private static int disks = 0;
+    private static String baseConfiguration = "0 0 0";
+    private static final String OUTPUT_PATH = "src/output/output.txt";
+
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fileOS = new FileOutputStream(OUTPUT_PATH);
+        PrintStream printer = new PrintStream(fileOS);
+
+        readInput();
+    }
+
+    public static void readInput() throws IOException {
+        BufferedReader jIn = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter jOut = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int cases = Integer.parseInt(jIn.readLine());
+        StringBuilder input = new StringBuilder();
+
+        for (int i = 0; i < cases; i++) {
+            input.append(jIn.readLine()).append(" ");
+        }
+
+        jOut.write(input.toString());
+
+        jIn.close();
+        jOut.close();
     }
 }
